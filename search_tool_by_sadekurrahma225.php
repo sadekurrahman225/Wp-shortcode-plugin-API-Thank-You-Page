8c99471f6f2d7d80018768bdfc0547b4aec8f820
Sadekur Rahman <spondon2255@gmail.com>
2021-08-10 02:06:09 +0600

    Code Cleaning Done

--- search_tool_by_sadekurrahma225.php
+++ search_tool_by_sadekurrahma225.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Plugin Name: WP Plugin by SadekurRahma225
+ * Plugin Name: Thank You Page by SadekurRahma225
  * Plugin URI: https://www.fiverr.com/users/sadekurrahma225/
- *Description: A Booking search tool in English Developed by Sadekurrahma225, A 5 Star Rated Developer on Fiverr and Upwork. Shortcode is search_form_en
+ *Description: Custom Thank You Page
  *Version: 1.0
  *Author: Sadekur Rahman
  *Author URI: https://www.linkedin.com/in/sadekur-rahman-a9b149143/
@@ -16,59 +16,18 @@ if (!function_exists('add_action')) {
     exit;
 }
 
-add_action('init', 'search_form_init');
+add_action('init', 'sadek_shortcode_function');
 
-function search_form_init()
+function sadek_shortcode_function()
 {
-    add_shortcode('search_form_en', 'search_form_en_function');
+    add_shortcode('sadek_client_data', 'sadek_function');
 }
 
-function search_form_en_function()
+function sadek_function()
 {
-    //     $html = '<div >
-    //         <div >
-    // 	        <div >
-    // 	        	<form id="formid"  name="order" class="form-inline"  action="">
-    // 		            <div >
-    // 		                <div >
-    //                             <label class="STinline">Checkin Date</label>
-    // 		                    <input class="STinline" id="checkInDate" type="text" name="checkInDate" value=""  />
+    
 
-
-    //                             <label class="STinline">Checkout Date</label>
-    //                             <input class="STinline" id="checkOutDate" type="text" name="checkOutDate" value=""  />
-
-
-
-    //                             <button class="STinline" id="search_button" type="submit" >
-    //                                 BOOK NOW
-    //                             </button>
-    // 		                </div>
-    // 		            </div>
-    // 		        </form>
-    // 	        </div>
-    // 	    </div>
-    // 	</div>
-    // </form>';
-
-    $html = '<form id="formid" class="form-inline" name="order" >
-
-  <label id="ST_title_1" class="mr-sm-2">Check IN </label>
-        <input id="checkInDate" type="text" name="checkInDate" value=""  class="form-control mb-2 mr-sm-2" >
-        
-                <span id="ST_calendar" class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-        
-  <label id="ST_title_2" class="mr-sm-2">Check OUT</label>
-        <input id="checkOutDate" type="text" name="checkOutDate" value=""  class="form-control mb-2 mr-sm-2" >
-
-                <span id="ST_calendar" class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-        
-  <button id="search_button" type="submit" >BOOK NOW</button>
-  </form>';
+    $html = '';
 
     return
         $html;
@@ -97,5 +56,3 @@ function custom_js()
 }
 
 
-//<input  id="checkInDate"  type="text" name="checkInDate" value="" onfocus ="(this.type=&#39date&#39)">
-//<input id="checkOutDate"  type="text" name="checkOutDate" value="" onfocus ="(this.type=&#39date&#39)"/>
